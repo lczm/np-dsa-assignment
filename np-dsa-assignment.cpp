268418ff9c5237ce2d2bbaeb8e9b8ebdf39af239
Abicharan <abicharan.b@gmail.com>
2021-01-30 12:19:32 +0800

    Added simple menu

--- np-dsa-assignment.cpp
+++ np-dsa-assignment.cpp
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+//Global variables in main
+Graph graph;
+Dictionary<Node*> dic;
+Vector<MrtLine> mrtlines;
+
 void testVector()
 {
     Vector<int> v(10, 1);
@@ -96,145 +101,202 @@ void testVector()
     cout << "---" << endl;
 }
 
-void testGraphConnections()
-{
-    Graph graph;
-    Dictionary<Node*> dic;
-    Vector<MrtLine> mrtlines;
-    graph.setNodeList(&dic);
-    FileReader filereader(&graph, &dic, mrtlines);
-    Vector<Connection*> c;
+//void testGraphConnections()
+//{
+//    Graph graph;
+//    Dictionary<Node*> dic;
+//    Vector<MrtLine> mrtlines;
+//    graph.setNodeList(&dic);
+//    FileReader filereader(&graph, &dic, mrtlines);
+//    Vector<Connection*> c;
+//
+//
+//    graph.removeConnection("EW13", "EW8");
+//    //graph.getAllConnectionsForNode("EW13", c);
+//
+//    Vector<Connection*> d;
+//   // graph.getAllConnectionsForNode("EW2", d);
+//
+//    cout << "EW13 connections: ";
+//    for (uint32_t i = 0; i < c.size(); i++)
+//    {
+//        cout << c[i]->toNode->id << " ";
+//    }
+//    cout << " " << endl;
+//    cout << "EW2 connections: ";
+//    for (uint32_t i = 0; i < d.size(); i++)
+//    {
+//        cout << d[i]->toNode->id << endl;
+//    }
+//
+//    cout << "\n";
+//
+//    cout << "mrt size:" <<  mrtlines.size() << endl;
+//    cout << "mrt name:" << mrtlines[0].getMrtLineName() << endl;
+//    graph.getAllGraphConnections(d);
+//
+//    cout << "d size: " << d.size() << endl;
+//    mrtlines[0].addNewStation("EW13", "test", true, 10, 10);
+//    mrtlines[0].addNewStation("EW13", "test2", false, 10, 10);
+//    cout << "mrt name:" << mrtlines[0].getMrtLineName() << endl;
+//    mrtlines[0].printStationsAll();
+//
+//    graph.getAllGraphConnections(c);
+//    cout << "c size: " << c.size() << endl;
+//    for (uint32_t i = 0; i < c.size(); i++)
+//    {
+//        cout << c[i]->fromNode->id << " ";
+//        cout << c[i]->toNode->id << " ";
+//    }
+//
+//    mrtlines[0].removeStation("EW1", 10);
+//    mrtlines[0].printStationsAll();
+//
+//     Vector<Connection*> f;
+//    graph.getAllGraphConnections(f);
+//    cout << "c size: " << f.size() << endl;
+//    for (uint32_t i = 0; i < f.size(); i++)
+//    {
+//        cout << f[i]->fromNode->id << " ";
+//        cout << f[i]->toNode->id << " ";
+//    }
+//}
+//
+//void testDoublyLinkedList()
+//{
+//    Node * fromNode = new Node();
+//
+//    // has interchange connections
+//    fromNode->id = "EW13";
+//
+//
+//    Node* fromNodeAnother = new Node();
+//
+//    // has interchange connections
+//    fromNodeAnother->id = "EW14";
+//
+//    Node toNode;
+//    toNode.id = "EW2";
+//
+//    Node toNode2;
+//    toNode2.id = "EW4";
+//
+//    DoublyLinkedList<Node> list;
+//    list.addFront(toNode);
+//    cout <<"size of doubly: " << list.getSize() << endl;
+//    list.remove(toNode);
+//    cout << "size of doubly: " << list.getSize() << endl;
+//   
+//     list.addFront(toNode);
+//     Node node = list.getAt(0);
+//     cout << node.id << endl;
+//
+//
+//    DoublyLinkedList<Node*> listptr;
+//    listptr.addFront(fromNode);
+//    cout << "size of doubly: " << listptr.getSize() << endl;
+//    listptr.remove(fromNode);
+//    cout << "size of doubly: " << listptr.getSize() << endl;
+//
+//    listptr.addFront(fromNode);
+//    listptr.addFront(fromNodeAnother);
+//    cout << "size of doubly: " << listptr.getSize() << endl;
+//    listptr.removeAt(0);
+//    cout << "size of doubly: " << listptr.getSize() << endl;
+//
+//    Node * getNode = listptr.getAt(0);
+//    cout << getNode->id << endl;
+//
+//}
+//
+//void testDictionary()
+//{   
+//    Node* fromNodeAnother = new Node();
+//
+//    // has interchange connections
+//    fromNodeAnother->id = "EW14";
+//    Dictionary<Node*> d;
+//    d.add(fromNodeAnother->id, fromNodeAnother);
+//    Node* nodetest = d.get("EW14");
+//    cout << nodetest->id << endl;
+//
+//
+//    Node toNode;
+//    toNode.id = "EW2";
+//    Dictionary<Node> nonPtr;
+//    nonPtr.add(toNode.id, toNode);
+//    Node nodetest2 = nonPtr.get("EW2");
+//    cout << nodetest2.id << endl;
+//}
 
 
-    graph.removeConnection("EW13", "EW8");
-    //graph.getAllConnectionsForNode("EW13", c);
-
-    Vector<Connection*> d;
-   // graph.getAllConnectionsForNode("EW2", d);
-
-    cout << "EW13 connections: ";
-    for (uint32_t i = 0; i < c.size(); i++)
-    {
-        cout << c[i]->toNode->id << " ";
-    }
-    cout << " " << endl;
-    cout << "EW2 connections: ";
-    for (uint32_t i = 0; i < d.size(); i++)
-    {
-        cout << d[i]->toNode->id << endl;
-    }
-
-    cout << "\n";
-
-    cout << "mrt size:" <<  mrtlines.size() << endl;
-    cout << "mrt name:" << mrtlines[0].getMrtLineName() << endl;
-    graph.getAllGraphConnections(d);
-
-    cout << "d size: " << d.size() << endl;
-    mrtlines[0].addNewStation("EW13", "test", true, 10, 10);
-    mrtlines[0].addNewStation("EW13", "test2", false, 10, 10);
-    cout << "mrt name:" << mrtlines[0].getMrtLineName() << endl;
-    mrtlines[0].printStationsAll();
-
-    graph.getAllGraphConnections(c);
-    cout << "c size: " << c.size() << endl;
-    for (uint32_t i = 0; i < c.size(); i++)
-    {
-        cout << c[i]->fromNode->id << " ";
-        cout << c[i]->toNode->id << " ";
-    }
-
-    mrtlines[0].removeStation("EW1", 10);
-    mrtlines[0].printStationsAll();
-
-     Vector<Connection*> f;
-    graph.getAllGraphConnections(f);
-    cout << "c size: " << f.size() << endl;
-    for (uint32_t i = 0; i < f.size(); i++)
-    {
-        cout << f[i]->fromNode->id << " ";
-        cout << f[i]->toNode->id << " ";
-    }
-}
-
-void testDoublyLinkedList()
-{
-    Node * fromNode = new Node();
-
-    // has interchange connections
-    fromNode->id = "EW13";
-
-
-    Node* fromNodeAnother = new Node();
-
-    // has interchange connections
-    fromNodeAnother->id = "EW14";
-
-    Node toNode;
-    toNode.id = "EW2";
-
-    Node toNode2;
-    toNode2.id = "EW4";
-
-    DoublyLinkedList<Node> list;
-    list.addFront(toNode);
-    cout <<"size of doubly: " << list.getSize() << endl;
-    list.remove(toNode);
-    cout << "size of doubly: " << list.getSize() << endl;
-   
-     list.addFront(toNode);
-     Node node = list.getAt(0);
-     cout << node.id << endl;
-
-
-    DoublyLinkedList<Node*> listptr;
-    listptr.addFront(fromNode);
-    cout << "size of doubly: " << listptr.getSize() << endl;
-    listptr.remove(fromNode);
-    cout << "size of doubly: " << listptr.getSize() << endl;
-
-    listptr.addFront(fromNode);
-    listptr.addFront(fromNodeAnother);
-    cout << "size of doubly: " << listptr.getSize() << endl;
-    listptr.removeAt(0);
-    cout << "size of doubly: " << listptr.getSize() << endl;
-
-    Node * getNode = listptr.getAt(0);
-    cout << getNode->id << endl;
-
-}
-
-void testDictionary()
-{   
-    Node* fromNodeAnother = new Node();
-
-    // has interchange connections
-    fromNodeAnother->id = "EW14";
-    Dictionary<Node*> d;
-    d.add(fromNodeAnother->id, fromNodeAnother);
-    Node* nodetest = d.get("EW14");
-    cout << nodetest->id << endl;
-
-
-    Node toNode;
-    toNode.id = "EW2";
-    Dictionary<Node> nonPtr;
-    nonPtr.add(toNode.id, toNode);
-    Node nodetest2 = nonPtr.get("EW2");
-    cout << nodetest2.id << endl;
-}
-
-
+void displayMenu();
+void displayAllStations();
 int main()
 {
+    graph.setNodeList(&dic);
+    FileReader filereader(&graph, &dic, mrtlines);
     cout << "np-dsa-assignment" << endl;
 
     // testing purposes as there is no unit tests
     testVector();
-    testGraphConnections();
+    /*testGraphConnections();
     testDoublyLinkedList();
-    testDictionary();
+    testDictionary();*/
+    bool exit = false;
+    while (exit != true)
+    {
+        int option = 0;
+        while (option != -1)
+        {
+            displayMenu();
+            cout << "Enter option: "; 
+            cin >> option;
+            switch (option)
+            {   
+                case 6:
+                    displayAllStations();
+                    break;
+                default:
+                    cout << "Please select the correct option" << endl;
+                    break;
+            }
+        }
 
-   
+        cout << "Are you sure you want to exit? (y/n) " << endl;
+        char ans;
+        cin >> ans;
+        if (toupper(ans) == 'Y')
+        {
+            exit = true;
+        }
+        else
+        {
+            exit = false;
+        }
+    }
     return 0;
+}
+
+void displayMenu()
+{   
+    cout << "\n";
+    cout << "Welcome to Railway inc. (Enter -1 to log off) " << endl;
+    cout << "Please select an option " << endl;
+    cout << "Option 1: Add a train station" << endl;
+    cout << "Option 2: Remove a train station" << endl;
+    cout << "Option 3: Add an interchange between stations" << endl;
+    cout << "Option 4: Remove an interchange between stations" << endl;
+    cout << "Option 5: Find the shortest path between stations" << endl;
+    cout << "Option 6: Print all Mrt Lines and stations" << endl;
+    cout << "\n";
+}
+
+void displayAllStations()
+{
+    for (int i = 0; i < mrtlines.size(); i++)
+    {   
+        cout << "\n";
+        mrtlines[i].printStationsAll();
+    }
 }
