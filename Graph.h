8dbeec5a24bc5b45230110c80df6b3f0f4f64f41
Abicharan <abicharan.b@gmail.com>
2021-02-05 13:03:49 +0800

    Some comments for graph

--- Graph.h
+++ Graph.h
@@ -48,15 +48,64 @@ class Graph
         return nodeList;
     };
 
+    //Add a connection between two stations (E.g. EW1 to EW2 with a cost of 1000m)
+    // pre : fromNodeId, toNodeId
+    // post: Add the connection to the graph
+    // returns a boolean
     bool addConnection(string fromNodeId, string toNodeId, int cost);
+
+    //Remove all connections that a fromNode has to the toNode
+    // pre : fromNodeId
+    // post: Removes all connections from the fromNode to the toNode
+    // Returns a boolean
     bool removeAllConnectionsForNodeOneWay(string fromNodeId);
+
+    //Remove all connections that a fromNode has to the toNode (vice-versa)
+    // pre : fromNodeId
+    // post: Removes all the connections from the fromNode to the toNode
+    // and the connection from the toNode to the fromNode
     bool removeAllConnectionsForNodeBothWays(string fromNodeId);
+
+    // Removes the connection one way between these two nodes
+    // pre : fromNodeId, toNodeId
+    // post: bool
+    // Removes the connection from the fromNode to the toNode
     bool removeConnection(string fromNodeId, string toNodeId);
+
+    //Check if the two nodes has a connection
+    // pre : fromNodeId, toNodeId
+    // post: bool
+    // Returns true if the two nodes have a connection
     bool hasConnection(string fromNodeId, string toNodeId);
+
+    //Get the specific connection between these two nodes
+    // pre : fromNodeId, toNodeId
+    // post: None
+    // Returns a pointer to the connection
     Connection* getConnection(string fromNodeId, string toNodeId);
+
+    //Get the index of the connection specified
+    // pre : fromNodeId, toNodeId
+    // post: None
+    // Returns the index of the connection stored in the vector
     int  getConnectionIndex(string fromNodeId, string toNodeId);
+
+    // Get all the existing connections that the node has
+    // pre : fromNodeId, nodeConnections
+    // post: None
+    // Returns all the connections that a node has
     void getAllConnectionsForNode(string fromNodeId, Vector<Connection*>& nodeConnections);
+
+    //Get all of the graph's connections
+    // pre : graphConnections
+    // post: None
+    // Returns all the connections that the graph has
     void getAllGraphConnections(Vector<Connection*>& graphConnections);
+
+    //Get the shortest path between two nodes
+    // pre : Starting destination, end destination
+    // post: None
+    // Returns the shortest path between the two stations
     void shortestPathBetweenStations(string fromNodeId, string toNodeId,
                                                     Vector<Connection*> &cons);
     ~Graph();
