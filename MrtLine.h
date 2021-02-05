8a5a91aff8ccc00e04340d817fda351b9364e46e
Abicharan <abicharan.b@gmail.com>
2021-02-05 14:39:07 +0800

    Added more comments for mrtLine

--- MrtLine.h
+++ MrtLine.h
@@ -48,21 +48,38 @@ class MrtLine
     void addNewStation(string stationId, string newStationName, bool infront, int costPrev, int costForward);
 
     // Updates the ids of the stations
+    // Example, If I were to add a station between NS11 and NS12
+    // the new station will be NS12 and all the stations after it will have to update their ids by 1
     // pre : selectedStationId, decreaseOrIncreaseId
     // post: Updates the ids of the train stations by adding or removing one
     void updateLineIdsForStations(string selectedStationId, int decreaseOrIncreaseId);
 
     //Updates the connections between the affected stations
+    // pre : int selectedStationPtr, int oldStationIndex, int costPrev, int costForward, 
+    //            Node *newStation, int beforeAfter (Infront or before the selected station)
+    // post: Updates the costs between the stations and adds the new station to the DLL
     void updateConnBetweenStationsForAddNewStation(int selectedStationPtr, int oldStationIndex, int costPrev,
                                           int costForward, Node* newStation, int beforeAfter);
 
-    //Adds station to the front of the DLL
+    // Adds station to the front of the DLL
+    // pre : Node*& newNode 
+    // post: Adds the station to the front of the DLL
     void addStationFront(Node*& newNode);
 
-    //Adds station to the end of the DLL
+    // Adds station to the end of the DLL
+    // pre : Node*& newNode
+    // post: Adds the station to the back of the DLL
     void addStationBack(Node*& newNode);
 
     //Removes a station
+    // Example: NS11 <----Cost----> NS12(to be deleted) <----Cost---->NS13(Id to be updated)
+    // NS11<----costBetween---->NS12(updated ID)
+
+    // pre : stationId
+    // post: removes the station from the DLL and from the global dic
+    // all connections related to this station is also deleted
+    // cost is updated between the two stations that the deleted station was 
+    // connected with.
     void removeStation(string stationId, int costBetween);
 
     //Removes all connections between these two stations
